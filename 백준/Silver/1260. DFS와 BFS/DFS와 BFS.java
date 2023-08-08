b6baf0cb84ea96b2993f0a79e38655397a0e8e3e
Ahyoung Kim <130175561+AhyngK@users.noreply.github.com>
2023-08-08 20:26:59 +0900

    [Silver II] Title: DFS와 BFS, Time: 248 ms, Memory: 20152 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,124 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int nodes = sc.nextInt();
-        int edges = sc.nextInt();
-        int firstNode = sc.nextInt();
-        sc.nextLine();
 
-        // 배열 생성
-        int[][] graph = new int[nodes+1][nodes+1];
-        for (int i = 0; i < graph.length; i++) {
-            Arrays.fill(graph[i],0);
+    static StringBuilder sbForDFS = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int nodes = Integer.parseInt(st.nextToken());
+        int Edges = Integer.parseInt(st.nextToken());
+        int startNode = Integer.parseInt(st.nextToken());
+
+        List<List<Integer>> Nodes = new ArrayList<>();
+        for (int w = 0; w <= nodes; w++) {
+            List<Integer> oneNode = new ArrayList<>();
+            Nodes.add(oneNode);
         }
-        // 리스트 생성
-        List<List<Integer>> graphList = new ArrayList<>();
-        for (int i = 0; i < nodes+1; i++) {
-            List<Integer> temp = new ArrayList<>();
-            graphList.add(temp);
+        for (int i = 0; i < Edges; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            Nodes.get(a).add(b);
+            Nodes.get(b).add(a);
         }
-        // 입력 그래프 배열, 리스트에 저장
-        for (int i = 0; i < edges; i++) {
-            int node1 = sc.nextInt();
-            int node2 = sc.nextInt();
-            sc.nextLine();
-
-            // 배열에 저장
-            graph[node1][node2] = 1;
-            graph[node2][node1] = 1;
-
-            // 리스트에 저장
-            graphList.get(node1).add(node2);
-            graphList.get(node2).add(node1);
+        for (int i = 0; i < Nodes.size(); i++) {
+            Collections.sort(Nodes.get(i));
         }
 
-        // 방문 여부 체크할 배열
-        boolean[] check = new boolean[nodes+1];
-
-        // DFS 탐색 호출
+        boolean[] check = new boolean[Nodes.size()];
         Arrays.fill(check,false);
-        DFSRecursionArray(graph,firstNode,check);
-        System.out.println();
-
-//        Arrays.fill(check,false);
-//        DFSRecursionList(graphList,firstNode,check);
-//        System.out.println();
-
-        // BFS 탐색 호출
-        BFSArray(graph,firstNode);
-//        System.out.println();
-//        BFSList(graphList,firstNode);
-
-        // 테스트 출력
-//        for (int i = 0; i < graph.length; i++) {
-//            System.out.println(Arrays.toString(graph[i]));
-//        }
-//        System.out.println();
-//        for (int i = 0; i < graphList.size(); i++) {
-//            System.out.println(graphList.get(i));
-//        }
+        check[startNode] = true;
+        check[0] = true;
+        DFS(Nodes,startNode,check);
+        System.out.println(sbForDFS);
+        System.out.println(BFS(Nodes,startNode));
     }
-
-    // DFS
-    static void DFSRecursionArray(int[][] graph, int start, boolean[] check){
-        check[start] = true;
-        System.out.print(start+" ");
-
-        for (int i = 0; i < graph[start].length; i++) {
-            if(graph[start][i]==1 && check[i]==false){
-                DFSRecursionArray(graph,i,check);
-            }
-        }
-    }
-    static void DFSRecursionList(List<List<Integer>> graphList, int start, boolean[] check){
-        check[start] = true;
-        System.out.print(start+" ");
-        // list에는 연결된 노드가 바로 들어있기 때문에 그 부분을 유의
-        for (int i = 0; i < graphList.get(start).size(); i++) {
-            if(check[graphList.get(start).get(i)] == false){
-                DFSRecursionList(graphList,graphList.get(start).get(i),check);
-            }
-        }
-    }
-
-    static void BFSArray(int[][] graph, int start){
+    static String BFS(List<List<Integer>> Nodes, int startNode){
         Queue<Integer> que = new LinkedList<>();
-        que.add(start);
-
-        boolean[] check = new boolean[graph.length];
+        StringBuilder sb = new StringBuilder();
+        boolean[] check = new boolean[Nodes.size()];
         Arrays.fill(check,false);
-        check[start] = true;
+
+        que.add(startNode);
+        check[startNode] = true;
+        check[0] = true;
+        sb.append(startNode).append(" ");
 
         while (!que.isEmpty()){
-            int first = que.poll();
-            System.out.print(first+" ");
-            for (int i = 0; i < graph[first].length; i++) {
-                if(graph[first][i]==1 && check[i]==false){
-                    check[i] = true;
-                    que.add(i);
+            int thisIndex = que.poll();
+            for (int i = 0; i < Nodes.get(thisIndex).size(); i++) {
+                if(!check[Nodes.get(thisIndex).get(i)]){
+                    que.add(Nodes.get(thisIndex).get(i));
+                    check[Nodes.get(thisIndex).get(i)] = true;
+                    sb.append(Nodes.get(thisIndex).get(i)).append(" ");
                 }
             }
         }
+        sb.append("\n");
+        return sb.toString();
     }
-    static void BFSList(List<List<Integer>> graphList, int start){
-        Queue<Integer> que = new LinkedList<>();
-        que.add(start);
-
-        boolean[] check = new boolean[graphList.size()];
-        Arrays.fill(check,false);
-        check[start] = true;
-
-        while (!que.isEmpty()){
-            int current = que.poll();
-            System.out.print(current+" ");
-
-            for (int i = 0; i < graphList.get(current).size(); i++) {
-                if(check[graphList.get(current).get(i)] == false){
-                    check[graphList.get(current).get(i)] = true;
-                    que.add(graphList.get(current).get(i));
-                }
+    static void DFS(List<List<Integer>> Nodes, int startNode, boolean[] check){
+        sbForDFS.append(startNode).append(" ");
+        for (int i = 0; i < Nodes.get(startNode).size(); i++) {
+            if(!check[Nodes.get(startNode).get(i)]){
+                check[Nodes.get(startNode).get(i)] = true;
+                DFS(Nodes,Nodes.get(startNode).get(i),check);
             }
         }
     }
