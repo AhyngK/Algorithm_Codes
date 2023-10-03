155e094035a47d329f28b651418f29548d18082f
Ahyoung Kim <130175561+AhyngK@users.noreply.github.com>
2023-10-03 21:17:21 +0700

    [Silver I] Title: 정수 삼각형, Time: 608 ms, Memory: 38124 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int triangleSize = sc.nextInt();
+        sc.nextLine();
+
+
+        ArrayList<long[]> triangle = new ArrayList<>();
+        for (int i = 0; i < triangleSize; i++) {
+            long[] temp1 = Arrays.stream(sc.nextLine().split(" ")).mapToLong(Long::parseLong).toArray();
+            triangle.add(temp1);
+        }
+        
+        ArrayList<long[]> addition = new ArrayList<>();
+        addition.add(triangle.get(0));
+
+        for (int i = 1; i <triangle.size() ; i++) {
+            long[] temp = new long[i+1];
+            for (int j = 0; j < i+1; j++) {
+                if(j==0){
+                    temp[j] = addition.get(0)[0]+triangle.get(i)[0];
+                }
+                else if (j==i) {
+                    temp[j] = addition.get(0)[j-1]+triangle.get(i)[j];
+                }
+                else {
+                    long temp2 = Math.max(addition.get(0)[j-1],addition.get(0)[j]);
+                    temp[j] = temp2+triangle.get(i)[j];
+                }
+            }
+            addition.remove(0);
+            addition.add(temp);
+        }
+
+        Arrays.sort(addition.get(0));
+        System.out.println(addition.get(0)[addition.get(0).length-1]);
+    }
+}
